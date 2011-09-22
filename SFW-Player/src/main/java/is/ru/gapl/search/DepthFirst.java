0dfaef9bcf33ff567f471efa3ed287c83563d5b6
Felix Rinker <Felix.Rinker@gmail.com>
2011-09-22 23:17:51 +0000

    Added the level counter

--- SFW-Player/src/main/java/is/ru/gapl/search/DepthFirst.java
+++ SFW-Player/src/main/java/is/ru/gapl/search/DepthFirst.java
@@ -12,53 +12,67 @@ public class DepthFirst implements ISearch{
 	public DepthFirst() {
 		
 	}
+	
+	
 	/**
 	 * 
 	 * @param node
 	 * @return
 	 */
 	public IMove bestMove(Node node) {
-		
+
 		int maxScore = 0;
 		ArrayList<ActionNodePair> actionList = node.getActionList();
 
-		ActionNodePair actionNode = actionList.get(0);
-		IMove bestAction = actionNode.getAction();
-	
+
+			ActionNodePair actionNode = actionList.get(0);
+			IMove bestAction = actionNode.getAction();
+            
+			int depth = 0;
+			long endTime = System.currentTimeMillis()+ 4950;
 			for(ActionNodePair aNP : actionList) {
-				
-				int score = maxScore(aNP.getNode());
+
+				int score = maxScore(aNP.getNode(), 1, endTime);
 				if(score == 100) { return aNP.getAction(); }
 				if(score > maxScore) { 
 					maxScore = score;
-					bestAction = aNP.getAction();	
+					bestAction = aNP.getAction();
 				}
 			}
-			
-		System.out.println("MaxScore:"+maxScore);	
+
 		return bestAction;
 	}
-	
+
 	/**
 	 * 
 	 * @param node
-	 * @param endTime 
 	 * @return
 	 */
-	private int maxScore( Node node ) {		
+	private int maxScore( Node node, int count, long endTime ) { 
+
+		int c = count;
 		
-		if( node.getScore() > -1 ) { return node.getScore(); }
-		if( node.getActionList().isEmpty() ) { return -1; }
-		
-		int maxScore = 0;
-		
-		for( ActionNodePair aNP : node.getActionList() ) {
+		if(System.currentTimeMillis() >= endTime) {
 			
-			int score = maxScore(aNP.getNode());
+			return -1; 
+		}
+		if (node.getScore() > -1) {
+			
+			return node.getScore();
+			}
+		if(node.getActionList().isEmpty()) {
+			
+			return -1;
+			}
+
+		int maxScore = 0;
+		c++;
+		for(ActionNodePair aNP : node.getActionList()) {
+
+			int score = maxScore(aNP.getNode(), c, endTime);
 			if(score == 100 || score == -1) { return score;}
 			if(score > maxScore) { maxScore = score;}
 		}
-		
 		return maxScore;
 	}
 }
