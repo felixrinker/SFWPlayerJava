0d6b904ceb239adf327cc8ff13bfd3ecb23f554f
Felix Rinker <Felix.Rinker@gmail.com>
2011-10-13 16:51:50 +0000

    Changed interface from parameter gameState to gameNode

--- SFW-Player/src/main/java/is/ru/gapl/search/ISearch.java
+++ SFW-Player/src/main/java/is/ru/gapl/search/ISearch.java
@@ -3,7 +3,7 @@ package is.ru.gapl.search;
 import is.ru.gapl.exception.PlayTimeOverException;
 import is.ru.gapl.exception.SearchMethodException;
 
-import org.eclipse.palamedes.gdl.core.model.IGameState;
+import org.eclipse.palamedes.gdl.core.model.IGameNode;
 import org.eclipse.palamedes.gdl.core.simulation.strategies.AbstractStrategy;
 
 /**
@@ -16,11 +16,11 @@ public interface ISearch {
 	/**
 	 * Provides the search
 	 * 
-	 * @param gameState the game state to start from
+	 * @param currentNode the game state to start from
 	 * @param strategy the strategy object
 	 * 
 	 * @throws PlayTimeOverException is thrown if the specified playtime is over
 	 * @throws SearchMethodException 
 	 */
-	public void search(IGameState gameState, AbstractStrategy strategy) throws SearchMethodException, PlayTimeOverException;
+	public void search(IGameNode currentNode, AbstractStrategy strategy) throws SearchMethodException, PlayTimeOverException;
 }
