28527ba51d128d4346b81e8fb8bd6e40d1c06ada
Felix Rinker <Felix.Rinker@gmail.com>
2011-11-07 03:10:44 +0000

    Correct the retrieving of the legal moves and the returning of the next move

--- SFW-Player/src/main/java/is/ru/gapl/search/MonteCarloSearch.java
+++ SFW-Player/src/main/java/is/ru/gapl/search/MonteCarloSearch.java
@@ -10,6 +10,7 @@ import org.eclipse.palamedes.gdl.core.model.IGame;
 import org.eclipse.palamedes.gdl.core.model.IGameState;
 import org.eclipse.palamedes.gdl.core.model.IMove;
 import org.eclipse.palamedes.gdl.core.model.IGameNode;
+import org.eclipse.palamedes.gdl.core.model.IReasoner;
 import org.eclipse.palamedes.gdl.core.simulation.Match;
 import org.eclipse.palamedes.gdl.core.simulation.strategies.AbstractStrategy;
 
@@ -33,6 +34,9 @@ public class MonteCarloSearch implements ISearch {
 	private Random generator;
 	private Random generator2;
 	private boolean debug = true;
+	private IReasoner reasoner;
+	private IGameState gameState;
+	private String ownRoleName;
 
 	@Override
 	public void init(AbstractStrategy strategy) {
@@ -40,60 +44,84 @@ public class MonteCarloSearch implements ISearch {
 		this.match			= this.strategy.getCurrentMatch();
 		this.game			= this.match.getGame();
 		this.ownRoleNum		= this.strategy.getOwnRoleNum();
+		this.ownRoleName	= this.strategy.getOwnRoleName();
+		this.reasoner		= this.strategy.getReasoner();
+		
+		this.generator = new Random();
 	}
 	
 	@Override
 	public void search(IGameNode currentNode, AbstractStrategy strategy)
 			throws SearchMethodException, PlayTimeOverException {
 		
-		this.generator = new Random(currentNode.getMoves().length);
-		//Monte Carlo uses two arrays of values:
-		//Q stores the values of the possible actions
-		//N stores the number of simulations which have been run for the possible actions
-		double[] Q = new double[currentNode.getMoves().length];
-		int[] N = new int[currentNode.getMoves().length];
+		
+		
+		
+			 MonteCarlo(currentNode);
+		
+	}
+	
+	private void MonteCarlo(IGameNode currentNode) throws PlayTimeOverException {
+		// Retrieves all legalMoves for own role
+		// from the current Node
+		IMove[] legalMoves = null;
+		try {
+			legalMoves	= game.getLegalMoves(currentNode)[this.ownRoleNum];
+		} catch (InterruptedException e1) {
+				debug("Was unable to retrieve the legal moves.");
+		}
+
+		// Monte Carlo uses two arrays of values:
+		// Q stores the values of the possible actions
+		// N stores the number of simulations which have been run for the possible actions
+		double[] Q	= new double[legalMoves.length];
+		int[]  N	= new int[legalMoves.length];
+				
 		//Both arrays are initially filled with 0's
-		for(int i=0; i<currentNode.getMoves().length; i++) {
+		for(int i=0; i<legalMoves.length; i++) {
 			Q[i]=0;
 			N[i]=0;
 		}
+				
+		double returnMoveValue	= Q[0];
 		
 		//While there is still time left
 		while(!this.strategy.isTimeUp()) {
 			//Randomly select a legal move
-			int index = generator.nextInt();
+			int index = generator.nextInt(legalMoves.length);
 			IMove[] action = new IMove[1];
-			action[0] = currentNode.getMoves()[index];
-			//Generate the next state
-			IGameNode nextNode = currentNode;
-			try {
-				nextNode = this.game.getNextNode(currentNode, action);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				debug("Was unable to calculate the next node.");
-			}
-			//Run a random simulation starting from the calculated node.
-			double score = runSimulation(nextNode);
-			//Now update the two arrays. The formula is in the slides of lecture 5.
-			Q[index] = (((Q[index]*N[index])+score)/(N[index]+1));
-			N[index] = N[index]+1;
+			action[0] = legalMoves[index];
+					
+			int ind = action[0].toString().indexOf("noop");
+			if( ind == -1 ) {	
+				//Generate the next state
+				IGameNode nextNode = null;
+				try {
+					nextNode = this.game.getNextNode(currentNode, action);
+				} catch (InterruptedException e) {
+						debug("Was unable to calculate the next node.");
+				}
+				
+				//Run a random simulation starting from the calculated node.
+				double score = runSimulation(nextNode);
+				//Now update the two arrays. The formula is in the slides of lecture 5.
+				Q[index] = (((Q[index]*N[index])+score)/(N[index]+1));
+				N[index] = N[index]+1;
+					
+				returnMoveValue = 0.0;
+				//Return the action which has the highest value at the moment.
+				for(int j = 0; j < legalMoves.length; j++) {
+					if(Q[j]>returnMoveValue) {
+						this.strategy.setBestMove(legalMoves[j]);
+						returnMoveValue = Q[j];
+					}
+				}
+					
+				debug("run simulation: "+N[index]+" for action: "+action[0]+" generated AVG score: "+Q[index]);
+			}	
 		}
-		
-		//Return the action which has the highest value at the moment.
-		IMove returnMove = currentNode.getMoves()[0];
-		
-		this.strategy.setBestMove(returnMove);
-		
-		double returnMoveValue = Q[0];
-		for(int j=1; j<currentNode.getMoves().length; j++) {
-			if(Q[j]>returnMoveValue) {
-				returnMove = currentNode.getMoves()[j];
-				returnMoveValue = Q[j];
-			}
-		}
-		
-		this.strategy.setBestMove(returnMove);
 	}
+	
 	
 	/**
 	 * Needs to run a random simulation until terminal state.
@@ -103,34 +131,51 @@ public class MonteCarloSearch implements ISearch {
 	 * @param gameNode
 	 * 
 	 * @return
+	 * @throws PlayTimeOverException 
 	 */
-	private double runSimulation(IGameNode gameNode) {
+	private double runSimulation(IGameNode gameNode) throws PlayTimeOverException {
+		
+		this.strategy.checkTime();
 		//If the node is terminal, the return the value.
 		if(gameNode.isTerminal()) {
-			return evaluateState( gameNode.getState(), this.ownRoleNum );
+			int score = 0;
+			try {
+				score = this.reasoner.getGoalValue(ownRoleName, gameNode.getState());
+			} catch (InterruptedException e) {
+				System.out.println("Error calculating the goal value");
+			}
+			
+			return score;
+		//	return evaluateState( gameNode.getState(), this.ownRoleNum );
 		}
 		else {
 			//Copied from the search method.
 			//It is supposed to pick a random move and generate
 			//the next state, so the method can be run again with
 			//this next state.
-			this.generator2 = new Random(gameNode.getMoves().length);
-			int index = generator2.nextInt();
+			IMove[] legalMoves = null;
+			try {
+				legalMoves = game.getLegalMoves(gameNode)[this.ownRoleNum];
+			} catch (InterruptedException e1) {
+				System.out.println("Was unable to retrieve the legal moves.");
+			}
+			
+			int index = generator.nextInt(legalMoves.length);
 			IMove[] action = new IMove[1];
-			action[0] = gameNode.getMoves()[index];
+			action[0] = legalMoves[index];
 			//Generate the next state
-			IGameNode nextNode = gameNode;
+			IGameNode nextNode = null;
 			try {
 				nextNode = this.game.getNextNode(gameNode, action);
 			} catch (InterruptedException e) {
-				debug("Was unable to calculate the next node.");
+				System.out.println("Was unable to calculate the next node.");
 			}
 			return runSimulation(nextNode);
 		}
 	}
 	
 	/**
-	 * opied this method from the Minimax, because it should
+	 * Copied this method from the Minimax, because it should
 	 * hopefully be the same.
 	 * 
 	 * @param gameState
